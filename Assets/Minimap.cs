6576addd2fbc814327c2706e055082e9f2463458
ghost <kbinas2022@elam.co.uk>
2024-02-22 17:03:49 +0000

    CYBERA

---
+++ Assets/Minimap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CyberMovementSystem
+{
+    public class Minimap : MonoBehaviour
+    {
+        public Transform player;
+
+        private void LateUpdate()
+        {
+            Vector3 newPosition = player.position;
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+        }
+    }
+}
